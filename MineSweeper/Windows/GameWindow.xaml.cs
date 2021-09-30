be959f0828e7ad1e577f1da6e9e3fc7dbbc2c140
Daan vam Geloven <482888@student.fontys.nl>
2021-09-30 22:12:27 +0200

    Added mainwindow redirect

---
+++ MineSweeper/Windows/GameWindow.xaml.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace MineSweeper
+{
+    /// <summary>
+    /// Interaction logic for GameWindow.xaml
+    /// </summary>
+    public partial class GameWindow : Window
+    {
+        private Random r;
+        private readonly int height;
+        private readonly int length;
+        private int bombs;
+        public GameWindow(int l, int h, int b)
+        {
+            InitializeComponent();
+            r = new Random();
+            height = h;
+            length = l;
+            bombs = b;
+            GenerateTiles();
+        }
+
+        private void GenerateTiles()
+        {
+            for (int row = 0; row < height; row++)
+            {
+                // generate row here
+                for (int col = 0; col < height; col++)
+                {
+                    // Generate Column here
+                }
+            }
+        }
+    }
+}
---
+++ MineSweeper/Windows/MainWindow.xaml.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace MineSweeper
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            GameWindow gw = new GameWindow(9, 9, 10);
+            gw.Show();
+            this.Close();
+        }
+
+        private void BtnIntermediate_Click(object sender, RoutedEventArgs e)
+        {
+            GameWindow gw = new GameWindow(16, 16, 40);
+            gw.Show();
+            this.Close();
+        }
+
+        private void BtnExpert_Click(object sender, RoutedEventArgs e)
+        {
+            GameWindow gw = new GameWindow(30, 16, 99);
+            gw.Show();
+            this.Close();
+        }
+
+        private void BtnCustom_Click(object sender, RoutedEventArgs e)
+        {
+            CustomGameWindow cgw = new CustomGameWindow();
+            cgw.Show();
+            this.Close();
+        }
+    }
+}
